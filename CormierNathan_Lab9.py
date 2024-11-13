21d26c9936dc1ed1b7bfffad2de2373fa3f82ac9
ncormie8 <ncormie8@uwo.ca>
2024-11-13 15:13:54 -0500

    Initial advection definition
    Initially defined the advection function with input parameters as shown.

--- CormierNathan_Lab9.py
+++ CormierNathan_Lab9.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 # Adapt the traffic.py code into a function that solves the advection equation for the time
 # evolution of ρ(x, t), using the Lax method with ρSL(x) as the initial condition. Integrate for
 # at least 1500 time steps using the time step � = h/vmax.
+
+def advection(grid_pts,sys_size,wv_spd,t_step,nstep):
+    return
