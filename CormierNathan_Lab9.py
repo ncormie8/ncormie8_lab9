2c6bab9072d76c9c49e6b861d434fecf3f9f7a04
ncormie8 <ncormie8@uwo.ca>
2024-11-13 15:22:24 -0500

    Initial parameter definition
    First attempt at adapting traffic.py parameters into advection function.

--- CormierNathan_Lab9.py
+++ CormierNathan_Lab9.py
@@ -6,5 +6,18 @@ import matplotlib.pyplot as plt
 # evolution of ρ(x, t), using the Lax method with ρSL(x) as the initial condition. Integrate for
 # at least 1500 time steps using the time step � = h/vmax.
 
-def advection(grid_pts,sys_size,wv_spd,t_step,nstep):
+def advection(grid_pts,sys_size,v_max):
+
+
+    # Defining parameters
+    N = grid_pts       # number of grid points
+    L = sys_size       # system size (m)
+    h = L/N            # grid spacing for BCs
+    t_step = h/v_max   # setting timestep to recommended value
+    n_step = (L/4)/(v_max*t_step)  # last car stops moving after n_step
+    coeff = t_step/2*h # coefficient used by lax method
+
+
+
+
     return
