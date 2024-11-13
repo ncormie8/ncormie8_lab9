19642302917e6bbc56c254868ba3748d59d20ceb
ncormie8 <ncormie8@uwo.ca>
2024-11-13 15:01:28 -0500

    imports and part 1 problem statement
    - imported necessary libraries to adapt traffic.py code
    - Commented in the part 1 problem statement

---
+++ CormierNathan_Lab9.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Part 1
+# Adapt the traffic.py code into a function that solves the advection equation for the time
+# evolution of ρ(x, t), using the Lax method with ρSL(x) as the initial condition. Integrate for
+# at least 1500 time steps using the time step � = h/vmax.
