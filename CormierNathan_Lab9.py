8d64d4dac3c199f83722c37ee4eefa68caedad34
ncormie8 <ncormie8@uwo.ca>
2024-11-13 16:19:24 -0500

    Start of logical adaptation from traffic.py
    - Initialized rho and flow arrays
    - Incorporated stoplight initial conditions for rho (1st attempt)
    - Copied periodic boundary conditions from traffic.py to be used in the numerical solving process

--- CormierNathan_Lab9.py
+++ CormierNathan_Lab9.py
@@ -11,6 +11,7 @@ vm = 25        # given v_max = 25
 rho_m = 1      # given rho_max = 1
 length = 1200  # length of x grid from -600 to + 600 (given)
 divisions = 600  # number of equally divided steps (given)
+rho_y0 = [rho_m,0] # definition of the initial condition vector for rho(x,0)
 
 def advection(grid_pts,sys_size,v_max,rho_sl):
     '''Solves the advection equation for the 
@@ -26,6 +27,24 @@ def advection(grid_pts,sys_size,v_max,rho_sl):
     t_step = h/v_max   # setting timestep to recommended value
     n_step = 1500      # minimum number of steps for integration
     coeff = t_step/2*h # coefficient used by lax method
+    p_max = rho_sl[0]  # setting the max value of rho to 1st index of input initial condtions
+
+    # Initializing density and flow arrays
+    rho = np.zeros(shape=(N))
+    flow = np.zeros(shape=(N))
+
+    # Incorporating stoplight initial condtions
+    rho[int(N/4):int(N/2)] = p_max
+    rho[0:N/4],rho[N/2:] = rho_sl[1]
+
+    # Periodic boundary conditions
+    ip = np.arange(N) + 1  
+    ip[N-1] = 0          # ip = i+1 with periodic b.c.
+    im = np.arange(N) - 1  
+    im[0] = N-1          # im = i-1 with periodic b.c.
+    
+    # Incorporating initial conditions
+
 
 
 
